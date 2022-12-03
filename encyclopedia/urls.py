5d24a2eb33e43247e68986e40d8c22546ea0b6c6
sworddut <90771969+sworddut@users.noreply.github.com>
2022-12-03 17:00:24 +0800

    Add files via upload

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("detail/<str:name>/",views.entry,name="entry"),
+    path("newPage/",views.newPage,name="newPage"),
+    path("search/",views.search,name="search"),
+    path("randomPage/",views.randomPage,name="randomPage"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,52 @@
 from django.shortcuts import render
-
 from . import util
+from django.http import HttpResponse
+import random
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+    if(len(request.GET.dict()) == 2):
+        title = request.GET.get('title')
+        content = request.GET.get('content')
+        print('title:',title,'content:',content)
+        util.save_entry(title,content)
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries(),
+            "selected":'CSS'
+        })
+    elif(len(request.GET.dict()) == 0):
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries(),
+            "selected":'CSS'
+        })
+    else:
+        return HttpResponse(404)
+
+def entry(request,name):
+    print(type(util.get_entry(name)))
+    response = render(request,"encyclopedia/detail.html",{
+        'thing':name,
+        'file':util.get_entry(name)
     })
+    response["Access-Control-Allow-Origin"] = '*'
+    return response
+
+def newPage(request):
+    return render(request,"encyclopedia/newPage.html")
+
+def search(request):
+    searchTitle = request.GET.get('q')
+    titles = util.list_entries()
+    newTitles = [title for title in titles if title == searchTitle]
+    return render(request, "encyclopedia/search.html", {
+        "entries":newTitles,
+        'titleLenth':newTitles.__len__
+    })
+
+def randomPage(request):
+    return render(request,"encyclopedia/randomPage.html",{
+        'selected':random.sample(util.list_entries(),1)
+    })
+
+
 
